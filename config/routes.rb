99c4e4d0bea2c4c4569ea13da45fe46c566aac59
Atbin <atbinjahandideh@gmail.com>
2017-10-22 23:53:16 +0000

    complete authentication system

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
-  get '/user/[.:format]', to: 'user#show'
+  get '/user/:id', to: 'user#show'
+  
   resources :articles
   
   get 'signup', to: 'users#new'
   post 'users', to: 'users#create'
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
